9fecf729800c5c772b5ea39f884e476da93ccb89
FiloPL <tzegarlicki@gmail.com>
2022-07-28 10:24:58 +0200

    Initial commit

---
+++ src/main/java/ttsw/filopl/cleanjavaarchitecture/CleanJavaArchitectureApplication.java
@@ -0,0 +1,13 @@
+package ttsw.filopl.cleanjavaarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanJavaArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanJavaArchitectureApplication.class, args);
+    }
+
+}
